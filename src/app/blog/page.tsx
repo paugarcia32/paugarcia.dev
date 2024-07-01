ce5ed0895ebd8bbc773eff8d8253696fb6df8544
Pau Garcia <paugarcia32@gmail.com>
2024-07-02 00:44:54 +0200

    solved stuff

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,20 +1,31 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import PostContent from '@/components/PostContent';
-import { PostMetadata } from '@/components/PostMetadata';
+import React, { useState, useEffect } from "react";
+import PostContent from "@/components/PostContent";
+import { PostMetadata } from "@/components/PostMetadata";
 
 const BlogPage: React.FC = () => {
   const [posts, setPosts] = useState<PostMetadata[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-      const response = await fetch(`${baseUrl}/api/posts`, {
-        cache: 'no-store'
-      });
-      const data = await response.json();
-      setPosts(data);
+      try {
+        const baseUrl = "https://paugarcia.dev";
+        // process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+        const response = await fetch(`${baseUrl}/api/posts`, {
+          cache: "no-store",
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to fetch: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setPosts(data);
+      } catch (error: any) {
+        setError(error.message);
+      }
     };
 
     fetchPosts();
@@ -22,7 +33,7 @@ const BlogPage: React.FC = () => {
 
   return (
     <main className="mx-auto">
-      <PostContent posts={posts} />
+      {error ? <div>Error: {error}</div> : <PostContent posts={posts} />}
     </main>
   );
 };
