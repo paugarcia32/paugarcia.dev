4a4926625293417915a0e67ce4975622649fa9bf
Pau Garcia <paugarcia32@gmail.com>
2024-07-01 20:57:48 +0200

    blog page formatting done

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,9 +2,9 @@ import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getPostMetadata from "@/lib/getPostMetadata";
-import TOC from "@/components/TOC"
-import { SearchIcon, TagIcon } from '@/components/icons';
-import { Separator } from "@/components/ui/separator"
+import TOC from "@/components/TOC";
+import { SearchIcon, TagIcon } from "@/components/icons";
+import { Separator } from "@/components/ui/separator";
 import DownloadButton from "@/components/downloadButton";
 import { PostMetadata } from "@/components/PostMetadata";
 import { PostCard } from "@/components/cards";
@@ -25,10 +25,11 @@ export const generateStaticParams = async () => {
   }));
 };
 
-
-
-
-const RelatedPostsSection = ({ relatedPosts }: { relatedPosts: PostMetadata[] }) => {
+const RelatedPostsSection = ({
+  relatedPosts,
+}: {
+  relatedPosts: PostMetadata[];
+}) => {
   return (
     <div className="mt-10 text-center items-center">
       <h1 className="text-2xl font-title font-semibold mb-4">Related Posts</h1>
@@ -44,12 +45,11 @@ const RelatedPostsSection = ({ relatedPosts }: { relatedPosts: PostMetadata[] })
 const getRelatedPosts = (tags: string[], currentSlug: string) => {
   const allPosts = getPostMetadata();
   const relatedPosts = allPosts.filter(
-    (post) => post.slug !== currentSlug && post.tags.some((tag) => tags.includes(tag))
+    (post) =>
+      post.slug !== currentSlug && post.tags.some((tag) => tags.includes(tag)),
   );
   return relatedPosts.slice(0, 3);
 };
-
-
 
 const PostPage = (props: any) => {
   const slug = props.params.slug;
@@ -57,9 +57,8 @@ const PostPage = (props: any) => {
   const relatedPosts = getRelatedPosts(post.data.tags, slug);
 
   return (
-    <div className="w-full">
+    <div className="w-full p-4">
       <div className="">
-
         <div className="my-6 text-center items-center">
           <h1 className="text-2xl font-title mb-4">{post.data.title}</h1>
           <div className="max-w-md mx-auto text-center">
@@ -69,45 +68,49 @@ const PostPage = (props: any) => {
         </div>
 
         <div className="md:flex md:mt-10">
-
-          <div className="md:w-1/4">
-            <div className="md:sticky md:top-12">
+          <div className="md:w-1/4 ">
+            <div className="md:sticky md:top-16 md:mt-12">
               <div className="flex justify-start items-center gap-2 flex-wrap mb-4">
                 <TagIcon />
-                {post.data.tags && post.data.tags.length > 0 && post.data.tags.map((tag: string, index: number) => (
-                  <Badge key={index} className="bg-accent-50 border border-accent-400 dark:bg-accent-950 dark:border-accent-600">
-                    <h1 className="font-semibold">{tag}</h1>
-                  </Badge>
-                ))}
+                {post.data.tags &&
+                  post.data.tags.length > 0 &&
+                  post.data.tags.map((tag: string, index: number) => (
+                    <Badge
+                      key={index}
+                      className="bg-accent-50 border border-accent-400 dark:bg-accent-950 dark:border-accent-600"
+                    >
+                      <h1 className="font-semibold">{tag}</h1>
+                    </Badge>
+                  ))}
               </div>
               <TOC />
             </div>
           </div>
 
-
           <div className="md:w-3/4 md:pl-8">
-            <article
-            className="prose dark:prose-invert prose:font-body prose-headings:font-title prose-headings:font-bold prose-h2:text-2xl prose-h2:mt-7.5 prose-h2:mb-2.5 prose-h3:text-xl prose-h3:mt-6 prose-h3:mb-2 prose-h4:text-lg prose-h4:mt-5 prose-h4:mb-1.5 prose-a:text-accent hover:prose-a:underline prose-a:no-underline prose-img:mx-auto"
-            >
-            <Markdown>{post.content}</Markdown>
-          </article>
+            <article className="prose dark:prose-invert prose:font-body prose-headings:font-title prose-headings:font-bold prose-h2:text-2xl prose-h2:mt-7.5 prose-h2:mb-2.5 prose-h3:text-xl prose-h3:mt-6 prose-h3:mb-2 prose-h4:text-lg prose-h4:mt-5 prose-h4:mb-1.5 prose-a:text-accent hover:prose-a:underline prose-a:no-underline prose-img:mx-auto">
+              <Markdown>{post.content}</Markdown>
+            </article>
           </div>
-
-
         </div>
         <div className="font-body mb-10 items-center text-center">
-            <Separator className="my-4" />
-            <p className="mb-4">If you liked this post, you can download it in PDF format!</p>
-            <div className="">
-              <DownloadButton slug={slug} />
-            </div>
+          <Separator className="my-4" />
+          <p className="mb-4">
+            If you liked this post, you can download it in PDF format!
+          </p>
+          <div className="">
+            <DownloadButton slug={slug} />
           </div>
-          <div className="m-0 pb-4 mx-auto leading-relaxed px-1.5 md:px-10 lg:px-20">
-            {relatedPosts.length > 0 && <RelatedPostsSection relatedPosts={relatedPosts} />}
-          </div>
+        </div>
+        <div className="m-0 pb-4 mx-auto leading-relaxed px-1.5 md:px-10 lg:px-20">
+          {relatedPosts.length > 0 && (
+            <RelatedPostsSection relatedPosts={relatedPosts} />
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
 export default PostPage;
+
--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { PostMetadata } from '@/components/PostMetadata';
-import { PostCard } from '@/components/cards';
-import { Hash  } from 'lucide-react';
+import React, { useState } from "react";
+import { PostMetadata } from "@/components/PostMetadata";
+import { PostCard } from "@/components/cards";
+import { Hash } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -10,7 +10,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import PaginationSection from '@/components/PaginationSection';
+import PaginationSection from "@/components/PaginationSection";
 
 interface PostContentProps {
   posts: PostMetadata[];
@@ -19,23 +19,24 @@ interface PostContentProps {
 const PAGE_SIZE = 6;
 
 const PostContent: React.FC<PostContentProps> = ({ posts }) => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [selectedTag, setSelectedTag] = useState<string | undefined>('');
+  const [selectedTag, setSelectedTag] = useState<string | undefined>("");
 
-  const allTags = Array.from(new Set(posts.flatMap(post => post.tags)));
+  const allTags = Array.from(new Set(posts.flatMap((post) => post.tags)));
 
   const handleTagChange = (value: string) => {
-    setSelectedTag(value === '' ? undefined : value);
+    setSelectedTag(value === "" ? undefined : value);
     setCurrentPage(1);
   };
 
-  const filteredPosts = posts.filter(post => {
+  const filteredPosts = posts.filter((post) => {
     const matchesSearchTerm =
       post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
       post.subtitle.toLowerCase().includes(searchTerm.toLowerCase());
 
-    const matchesSelectedTag = !selectedTag || selectedTag === '' || post.tags.includes(selectedTag);
+    const matchesSelectedTag =
+      !selectedTag || selectedTag === "" || post.tags.includes(selectedTag);
 
     return matchesSearchTerm && matchesSelectedTag;
   });
@@ -46,10 +47,10 @@ const PostContent: React.FC<PostContentProps> = ({ posts }) => {
   const totalPages = Math.ceil(filteredPosts.length / PAGE_SIZE);
 
   return (
-    <div className='max-w-6xl'>
-      <div className='flex justify-center'>
+    <div className="max-w-6xl">
+      <div className="flex justify-center">
         <Input
-          className='mb-4 max-w-xl mx-8'
+          className="mb-4 max-w-xl mx-8"
           type="text"
           placeholder="Search Posts ..."
           onChange={(e) => setSearchTerm(e.target.value)}
@@ -57,13 +58,10 @@ const PostContent: React.FC<PostContentProps> = ({ posts }) => {
       </div>
       <div className="md:flex">
         <div className="md:w-1/4 mx-8 md:mx-4">
-          <div className="md:sticky md:top-12 md:mt-10">
-            <Select
-              value={selectedTag || ''}
-              onValueChange={handleTagChange}
-            >
+          <div className="md:sticky md:top-16 md:mt-12">
+            <Select value={selectedTag || ""} onValueChange={handleTagChange}>
               <SelectTrigger className="max-w-xl">
-                <Hash  className="text-2xl pointer-events-none flex-shrink-0 text-primary" />
+                <Hash className="text-2xl pointer-events-none flex-shrink-0 text-primary" />
                 <SelectValue placeholder="Filter by Tag" />
               </SelectTrigger>
               <SelectContent>
@@ -77,20 +75,20 @@ const PostContent: React.FC<PostContentProps> = ({ posts }) => {
           </div>
         </div>
         <div className="md:w-3/4 md:mx-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-2xl gap-4 my-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-2xl gap-1 my-10">
             {currentPosts.map((post) => (
               <PostCard key={post.slug} {...post} />
             ))}
           </div>
-          <div className="flex justify-center mb-6">
-            <PaginationSection
-              totalPosts={filteredPosts.length}
-              postsPerPage={PAGE_SIZE}
-              currentPage={currentPage}
-              setCurrentPage={setCurrentPage}
-            />
-          </div>
         </div>
+      </div>
+      <div className="flex justify-center mb-6">
+        <PaginationSection
+          totalPosts={filteredPosts.length}
+          postsPerPage={PAGE_SIZE}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </div>
   );
