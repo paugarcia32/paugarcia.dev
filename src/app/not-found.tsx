efe853ae1f458b13c01cc1c73863e39722074297
Pau Garcia <paugarcia32@gmail.com>
2024-07-22 23:45:47 +0200

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,41 +1,65 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
+
 // pages/not-found.tsx
 
 import Link from "next/link";
 import Layout from "@/components/style/Layout";
+import { Heading1, Heading3 } from "@/components/style/Headings";
+import { Button } from "@/components/ui/button";
+import { Contact, NotebookPen, PanelsTopLeft } from "lucide-react";
+import { LucideIcon } from "lucide-react";
+
+interface Redirect {
+  url: string;
+  text: string;
+  Icon: LucideIcon; // Use "Icon" instead of "icon" to follow React naming conventions
+}
+
+const redirects: Redirect[] = [
+  {
+    url: "/projects",
+    text: "My portfolio",
+    Icon: PanelsTopLeft,
+  },
+  {
+    url: "/blog",
+    text: "My blog page",
+    Icon: NotebookPen,
+  },
+  {
+    url: "/contact",
+    text: "Contact me",
+    Icon: Contact,
+  },
+];
 
 const NotFoundPage: React.FC = () => {
   return (
-    <main className="flex flex-col items-center justify-center max-h-screen min-h-[1000]">
-      <Layout title="404">
-        {" "}
-        <h1 className="text-4xl font-bold mb-8">404 - Not Found</h1>
-        <p className="text-lg mb-8">
-          The page you're looking for doesn't exist.
-        </p>
-        <div className="flex gap-4">
-          <Link
-            href="/"
-            className="bg-primary text-white py-2 px-4 rounded-md hover:bg-primary-dark transition duration-300"
-          >
-            Go to Home
-          </Link>
-          <Link
-            href="/blog"
-            className="bg-secondary text-white py-2 px-4 rounded-md hover:bg-secondary-dark transition duration-300"
-          >
-            Visit Blog
-          </Link>
-          <Link
-            href="/contact"
-            className="bg-accent text-white py-2 px-4 rounded-md hover:bg-accent-dark transition duration-300"
-          >
-            Contact Us
-          </Link>
+    <Layout title="Page Not Found">
+      <main className="flex flex-col items-center justify-center max-h-screen">
+        <div className="mb-10">
+          <Heading1>Oops</Heading1>
+          <Heading3>Page not found😞🤙</Heading3>
+          <p className="text-md">
+            Something went wrong. This page is missing or you assembled the link
+            incorrectly.
+          </p>
         </div>
-      </Layout>
-    </main>
+        <div className="">
+          <p className="text-lg mb-8">Maybe you were searching for...</p>
+          <div className="flex gap-4">
+            {redirects.map((event, index) => (
+              <Link key={index} href={event.url}>
+                <Button>
+                  <event.Icon className="mr-2" /> {event.text}
+                </Button>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
